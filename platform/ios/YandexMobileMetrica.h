0df3d069d318bca39dc91e512f7c9c4266986a17
abonec <abonec@gmail.com>
2016-01-13 13:03:46 +0300

    [#]fix imports

--- platform/ios/YandexMobileMetrica.h
+++ platform/ios/YandexMobileMetrica.h
@@ -9,7 +9,7 @@
  * You may obtain a copy of the License at http://legal.yandex.com/metrica_termsofuse/
  */
 
-#import <YMMYandexMetrica.h>
-#import <YMMVersion.h>
-#import <YMMYandexMetricaConfiguration.h>
-#import <YMMYandexMetricaPreloadInfo.h>
+#import "YMMYandexMetrica.h"
+#import "YMMVersion.h"
+#import "YMMYandexMetricaConfiguration.h"
+#import "YMMYandexMetricaPreloadInfo.h"
